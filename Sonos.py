2b9b4f5559bbb0e67fb379617be5f83a1edd978f
Psycho <laurentchervet@bluewin.ch>
2020-03-11 21:25:42 +0100

    Working copy

--- Sonos.py
+++ Sonos.py
@@ -1,4 +1,11 @@
+from typing import Dict
+
+import soco
+from soco import SoCo
+
+from core.ProjectAliceExceptions import SkillStartingFailed
 from core.base.model.AliceSkill import AliceSkill
+from core.commons import constants
 from core.dialog.model.DialogSession import DialogSession
 from core.util.Decorators import IntentHandler
 
@@ -9,6 +16,49 @@ class Sonos(AliceSkill):
 	Description: Let alice control your sonos hardware
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+	def __init__(self):
+		super().__init__()
+		self._sonosPlayers: Dict[str, SoCo] = dict()
+
+
+	def onStart(self):
+		super().onStart()
+
+		self._sonosPlayers = {device.player_name.lower(): device for device in soco.discover()}
+
+		if not self._sonosPlayers:
+			self.logWarning('No Sonos device found')
+			raise SkillStartingFailed
+
+
+	@IntentHandler('PlayStopPauseSonos')
+	def playStopPause(self, session: DialogSession, **_kwargs):
+		action = session.slots['Action'] if 'Action' in session.slots else 'play'
+		room = session.slots['Room'].lower() if 'Room' in session.slots else session.siteId
+
+		if room not in self._sonosPlayers and room != constants.EVERYWHERE:
+			self.endDialog(
+				sessionId=session.sessionId,
+				text=self.randomTalk(text='noPlayerInRoom', replace=[room])
+			)
+			return
+
+		if room == constants.EVERYWHERE:
+			for room in self._sonosPlayers:
+				self.action(room, action)
+		else:
+			self.action(room, action)
+
+		self.endDialog(
+			sessionId=session.sessionId,
+			text=self.randomTalk(text='ok')
+		)
+
+
+	def action(self, room: str, action: str):
+		if action == 'play':
+			self._sonosPlayers[room].play()
+		elif action == 'stop':
+			self._sonosPlayers[room].stop()
+		else:
+			self._sonosPlayers[room].pause()
