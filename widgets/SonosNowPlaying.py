7ac3b43a975dcdc20d0dad476e5ff7b23c9ca610
Psycho <laurentchervet@bluewin.ch>
2020-04-22 09:01:11 +0200

    update

--- widgets/SonosNowPlaying.py
+++ widgets/SonosNowPlaying.py
@@ -1,13 +1,13 @@
 import sqlite3
 
 from core.base.model.Widget import Widget
-from core.base.model.widgetSizes import WidgetSizes
+from core.base.model.WidgetSizes import WidgetSizes
 
 
 class SonosNowPlaying(Widget):
 
-	SIZE = WidgetSizes.w
-	OPTIONS: dict = dict()
+	DEFAULT_SIZE = WidgetSizes.w
+	DEFAULT_OPTIONS: dict = dict()
 
 	def __init__(self, data: sqlite3.Row):
 		super().__init__(data)
